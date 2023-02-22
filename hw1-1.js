4362abcac2ce2187e8374781e4a0705ac3360b75
JesusGopnique <plaksa.pavel7@gmail.com>
2023-02-22 16:04:45 +0300

    added JS homework

---
+++ hw1-1.js
@@ -0,0 +1,17 @@
+let age1 = 10;
+let age2 = 18;
+let age3 = 60;
+
+
+if(age1 < age2){
+    console.log('You do not have access. Because your age is ' + age1 + '. It is less than ' + age2)
+}
+else if(age1 > age && age1 < age3){
+    console.log('Welcome!')
+}
+else if(age1 > age3){
+    console.log('Keep calm and look Culture channel')
+}
+else{
+    console.log('Technical work')
+}
---
+++ hw1-2.js
@@ -0,0 +1,26 @@
+let age1 = 15;
+let age2 = 18;
+let age3 = 60;
+
+const checkAge = function(age){
+
+    if(age < age1){
+        console.log('You do not have acces. Because your age is ' + age + '. It is less than ' + age1);
+    }
+    else if(age >= age1 && age < age2){
+        console.log('You have a limited access');
+    }
+    else if(age >= age2 && age <= age3 ){
+        console.log('You can watch full content');
+    }
+    else if(age > age && age < 100){
+        console.log('Keep calm and watch "Culture channel"');
+    }
+    else{
+        console.log('You have to be dead')
+    }
+
+}
+checkAge(13)
+
+
