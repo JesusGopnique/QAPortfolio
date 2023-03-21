86766717535d26ec9152adf0f6aa59c44fd103cd
JesusGopnique <125078306+JesusGopnique@users.noreply.github.com>
2023-03-21 23:09:28 +0300

    Update hw1-1.js

--- hw1-1.js
+++ hw1-1.js
@@ -4,14 +4,14 @@ let age3 = 60;
 
 
 if(age1 < age2){
-    console.log('You do not have access. Because your age is ' + age1 + '. It is less than ' + age2)
+    console.log('You do not have access. Because your age is ' + age1 + '. It is less than ' + age2);
 }
 else if(age1 > age && age1 < age3){
-    console.log('Welcome!')
+    console.log('Welcome!');
 }
 else if(age1 > age3){
-    console.log('Keep calm and look Culture channel')
+    console.log('Keep calm and look Culture channel');
 }
 else{
-    console.log('Technical work')
+    console.log('Technical work');
 }
